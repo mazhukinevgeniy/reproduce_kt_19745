be5ddd86a27c1682f499d87575d218b1751845d3
Evgenii Mazhukin <evgenii.mazhukin@jetbrains.com>
2023-09-14 17:15:35 +0200

    1.9.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.20-Beta"
-    kotlin("kapt") version "1.8.0"
+    kotlin("jvm") version "1.9.0"
+    kotlin("kapt") version "1.9.0"
 }
 
 group = "org.example"
